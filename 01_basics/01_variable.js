73bc3211048504539482680b1dd841584c5b0745
VIKRAM <vikramkataria098@gmail.com>
2025-01-21 15:51:37 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,24 @@
+const addharId = 9610026027
+const myName = 'vikram'
+let emailId = "vikramkataria098@gmail.com"
+var emailPassword = "PASSWORD" // not use because issue in block and fuctional scope
+accountcity = "Nohar"   // ese bhi variable ko define kiya ja skta hai!
+let accountDistic
+//addharId = 838383883// Not Valid
+
+// these are the changeable // given bellow
+
+emailId = "vikramkumar18u@gmail.com"
+emailPassword = "hello buddy"
+accountcity = "bikaner"
+
+console.log(addharId);
+console.table([addharId, myName, emailId, emailPassword, accountcity, accountDistic])
+
+
+// agar javaScript me bina value ke variable ko define kr diya jaye to uski value output me "undefined" btayegi
+
+/*
+perfer to not to use "var" 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS-code as newer version
+
+// to find the type of data and 2 main datatype which are important 
+
+console.log(typeof null);      // object 
+console.log(typeof undefined); // undefined
+
+//alert(2+3) we use node js not brouser
+
+// number => 2 to power 53
+// bigint => in number is very big then it is used(tarding, stock_market)
+// string => can be use in '' and ""  ,most of the time used string is ""
+// boolean => true/false
+// null => standalone value
+// undefined => wihich is not defined till now
+// symbol => used for finding unique no or name
+
+// object 
+// note: null is a object type data 
+// question: how to find data type 
+// ans:  console.log(typeof vale_to_find_type);
+

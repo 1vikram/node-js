6a13ab304896a43e467300c7577d43a617ceeb25
VIKRAM <vikramkataria098@gmail.com>
2025-01-25 04:33:56 +0000

    conversionOperations

---
+++ 01_basics/04_conversionOperation.js
@@ -0,0 +1,21 @@
+let value01 = 19
+let negValue = -value01
+console.log(negValue);  // output is '-19'
+//console.log(2+3);
+//console.log(2-3);
+//console.log(2*3);
+//console.log(2/3);
+//console.log(2**3);
+//console.log(2%3);
+let str1 = "hello "
+let str2 = "vikram"
+console.log(str1+str2);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+//compartion (<,>,=,=!,<=,>=) output give as boolean (true,false) values
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
